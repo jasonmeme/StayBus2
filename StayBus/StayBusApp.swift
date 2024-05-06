056c61ab67102e5e3815173c7c11dbfb82d4091a
Jason Zhu <55606978+jasonmeme@users.noreply.github.com>
2024-05-06 08:42:02 -0400

    Initial Commit

---
+++ StayBus/StayBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayBusApp.swift
+//  StayBus
+//
+//  Created by Jason Zhu on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct StayBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
